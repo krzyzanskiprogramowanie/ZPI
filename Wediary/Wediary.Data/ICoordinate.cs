ec89ba719133387c6c9fce7e262f611c258fef9d
krzyzanskiprogramowanie <73827583+krzyzanskiprogramowanie@users.noreply.github.com>
2020-11-15 18:04:33 +0100

    Baza interfejsy czesc 2

---
+++ Wediary/Wediary.Data/ICoordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Wediary.Data.Models;
+
+namespace Wediary.Data
+{
+    public interface ICoordinate
+    {
+        Coordinate GetById(string id);
+        IEnumerable<Coordinate> GetAll();
+
+        Task Create(Coordinate applicationUser);
+        Task Delete(string id);
+        Task UpdateUser(string id);
+    }
+}
